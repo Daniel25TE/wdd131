536c6ee719749dd8ebf8b5b53840e68cb894331b
Daniel25TE <llu24001@byui.edu>
2025-03-08 17:19:22 -0700

    final week 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,5 @@
-// Get the current year and display it in the footer
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = currentYear;
 
-// Get the last modified date of the document
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
