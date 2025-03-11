9ece1de6171e5438f08b2fce3f6c6b2342ad04d7
Daniel25TE <llu24001@byui.edu>
2025-03-11 08:08:34 -0600

    martes day

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,15 +1,29 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
 
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
+button.addEventListener('click', function() {
+    
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
 
-deleteButton.textContent = '❌';
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
 
-li.append(deleteButton);
+        li.append(deleteButton);
+        list.append(li);
 
-list.append(li);
+        input.value = '';
+        input.focus();
+        
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+            
+        });
+    }
+});
