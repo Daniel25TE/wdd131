fca7387ce5e395ad50249f9b5314280337b8e880
Daniel25TE <llu24001@byui.edu>
2025-03-12 07:20:02 -0600

    wednesday day

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
